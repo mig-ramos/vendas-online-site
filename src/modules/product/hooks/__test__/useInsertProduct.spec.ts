eeb3190daba23399ba3bb6c689864da3e0247ca0
Miguel Arcanjo <mig.ramos10@gmail.com>
2023-10-03 18:32:39 -0300

    feat: test request in useInsertProduct

--- src/modules/product/hooks/__test__/useInsertProduct.spec.ts
+++ src/modules/product/hooks/__test__/useInsertProduct.spec.ts
@@ -1,20 +1,32 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { act, renderHook } from '@testing-library/react';
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
 
+import { URL_PRODUCT } from '../../../../shared/constants/urls';
 import { useInsertProduct } from '../useInsertProduct';
+
+const mockAxios = new MockAdapter(axios);
+
+mockAxios.onPost(URL_PRODUCT, {});
+
 const mockNavigate = jest.fn();
 const mockSetNotification = jest.fn();
+
 jest.mock('react-router-dom', () => ({
   useNavigate: () => mockNavigate,
 }));
+
 jest.mock('../../../../store/reducers/globalReducer/useGlobalReducer', () => ({
   useGlobalReducer: () => ({
     setNotification: mockSetNotification,
   }),
 }));
+
 describe('Test useInsertProduct', () => {
   it('should return initial values', () => {
     const { result } = renderHook(() => useInsertProduct());
+
     expect(result.current.loading).toEqual(false);
     expect(result.current.disabledButton).toEqual(true);
     expect(result.current.product).toEqual({
@@ -23,24 +35,32 @@ describe('Test useInsertProduct', () => {
       image: '',
     });
   });
+
   it('should change select in handleChangeSelect', () => {
     const { result } = renderHook(() => useInsertProduct());
+
     act(() => {
       result.current.handleChangeSelect('4324');
     });
+
     expect(result.current.product.categoryId).toEqual(4324);
   });
+
   it('should change product in onChangeInput send name', () => {
     const TEST_MOCK = 'TEST_MOCK';
     const { result } = renderHook(() => useInsertProduct());
+
     act(() => {
       result.current.onChangeInput({ target: { value: TEST_MOCK } } as any, 'name');
     });
+
     expect(result.current.product.name).toEqual(TEST_MOCK);
   });
+
   it('should change product in onChangeInput send price', () => {
     const TEST_MOCK = '3453';
     const { result } = renderHook(() => useInsertProduct());
+
     act(() => {
       result.current.onChangeInput({ target: { value: TEST_MOCK } } as any, 'price', true);
     });
@@ -82,5 +102,19 @@ describe('Test useInsertProduct', () => {
     });
 
     expect(result.current.disabledButton).toEqual(true);
+  });
+
+  it('should call axios.post', () => {
+    const spyAxios = jest.spyOn(axios, 'post');
+
+    const { result } = renderHook(() => useInsertProduct());
+
+    act(() => {
+      result.current.handleInsertProduct();
+    });
+
+    expect(spyAxios.mock.calls[0][1]).toEqual(result.current.product);
+
+    expect(spyAxios.mock.calls.length).toEqual(1);
   });
 });
